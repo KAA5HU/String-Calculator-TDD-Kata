e1e93aae59ef4a32e8b39e4005cb4a8529adfa52
Neeladatta <37861667+KAA5HU@users.noreply.github.com>
2024-09-02 15:36:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ function add(numbers) {
 
     let delimiter = /[\n,]/;  // Default delimiters: comma and newline
     if (numbers.startsWith("//")) {
+        // finding the \n where the numbers start
         const delimiterEndIndex = numbers.indexOf('\n');
         //regexp to find the delimiter
         delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
+        // delimiterEndIndex+1 will be the index where numbers string will start
         numbers = numbers.substring(delimiterEndIndex + 1);
     }
 
     const numArray = numbers.split(delimiter).map(num => parseInt(num, 10));
+    // filter all negative numbers 
     const negatives = numArray.filter(num => num < 0);
 
     if (negatives.length > 0) {
